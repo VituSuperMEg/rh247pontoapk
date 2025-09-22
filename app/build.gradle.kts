75d0f78d9863b83c1a11580ea6313523e6c2f13f
VituSuperMEg <vitorpires8555@gmail.com>
2025-09-22 09:39:55 -0300

    versao: 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ml.shubham0204.facenet_android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 40
-        versionName = "0.4.0"
+        versionCode = 41
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
